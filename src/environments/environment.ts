ef607dd7e17a6e2172dc3817e635d28c4d81ceb1
Ibe Sarah <ibesarah01@gmail.com>
2022-08-28 16:17:32 +0100

    all commits

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCMEHaTryfPnncogsvFqJUG0om_CVLgCkg",
+    authDomain: "blog-ada8c.firebaseapp.com",
+    databaseURL: "https://blog-ada8c-default-rtdb.firebaseio.com",
+    projectId: "blog-ada8c",
+    storageBucket: "blog-ada8c.appspot.com",
+    messagingSenderId: "802577048252",
+    appId: "1:802577048252:web:23647304fd908974bee6b9",
+    measurementId: "G-9JVTPJ8V8T"
+  }
 };
 
 /*
